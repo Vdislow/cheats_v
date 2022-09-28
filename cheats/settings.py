2a81d73dc846ade29fa3247403bd0d96d70c80f2
Vdislow <vladislavkahn@gmail.com>
2022-09-29 00:48:48 +0600

    her_ok

--- cheats/settings.py
+++ cheats/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0iw1-*6243kjy_!ro!4*h_zf^!#^m0$m6w%$jg%%=cb8snko72'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'chemax-v.herokuapp.com']
 
