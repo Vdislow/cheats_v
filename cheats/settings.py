7446af54c92d2de6043b4e1fc90f6cff307e31fc
Vdislow <vladislavkahn@gmail.com>
2022-09-29 00:55:46 +0600

    last try

--- cheats/settings.py
+++ cheats/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0iw1-*6243kjy_!ro!4*h_zf^!#^m0$m6w%$jg%%=cb8snko72
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'chemax-v.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'cheats-v.herokuapp.com']
 
 
 # Application definition
